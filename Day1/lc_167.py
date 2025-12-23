7e809bd91ebac180449447dca9e3d5203a442298
Suman Debnath <suman@suman-D96GJP2J0H.local>
2025-12-22 17:31:14 -0800

    day1

---
+++ Day1/lc_167.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        start = 0
+        end = len(numbers) - 1
+
+        while start < end:
+            present_sum = numbers[start] + numbers[end]
+            if present_sum == target:
+                return (start+1, end+1)
+            elif present_sum > target:
+                end -=1
+            elif present_sum < target:
+                start += 1
+        return  
+
